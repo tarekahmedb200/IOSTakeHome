dfe1e0a973d1e0baad8db7261c650beac30bbe5c
tarekahmed199 <tarek.ahmed@nandbox.com>
2023-05-11 00:20:23 +0300

    Initial Commit

---
+++ IOSTakeHome/IOSTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSTakeHomeApp.swift
+//  IOSTakeHome
+//
+//  Created by lapshop on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct IOSTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
